1ee2ff6df7edfd6ab322ddf99664acd7d935a1ad
HoneyLMT <honey.migueltaura@gmail.com>
2025-12-12 16:32:41 -0500

    change the canvas with a new one

--- Assets/Scripts/PlayerTrigger.cs
+++ Assets/Scripts/PlayerTrigger.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class PlayerTrigger : MonoBehaviour
 {
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Completed"))
         {
-            FindObjectOfType<GameManager>().ShowCompletionPanel();
+            if (gameManager != null)
+                gameManager.ShowCompletionPanel();
+            else
+                Debug.LogError("GameManager not found in the scene!");
         }
     }
 }
