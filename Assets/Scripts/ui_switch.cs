e083ed2e0fcebfe015f0b5e32362ae6d46497233
daniluna06 <dcl3581@uncw.edu>
2025-12-12 09:04:17 -0500

    deleted mesh (hands are now showing) .. recipe ui still does not show. cup is filling.. still not detecting recipes. adjusted some scripts.

--- Assets/Scripts/ui_switch.cs
+++ Assets/Scripts/ui_switch.cs
@@ -1,72 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class ui_switch : MonoBehaviour
+public class RecipeUIController : MonoBehaviour
 {
-    public GameObject Canvas;
-    private int index = 3;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject canvasRoot;     // your recipe canvas
+    public GameObject[] recipePanels; // size 4
+
+    private int recipeIndex = 0;
+
+    private void Start()
     {
-        Canvas.SetActive(false);
-        int count = Canvas.transform.childCount;
-
-        // Hide all children
-        for (int i = 0; i < count; i++)
-        {
-            Canvas.transform.GetChild(i).gameObject.SetActive(false);
-        }
-
-        // Start with the first child active
-        if (count > 0)
-        {
-            index = 0;
-            Canvas.transform.GetChild(index).gameObject.SetActive(true);
-        }
+        ShowRecipe(0);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowRecipe(int index)
     {
-        // 
-        if (Input.GetKeyDown(KeyCode.Space)) //test input
-        {
-            Canvas.SetActive(true);
-            int count = Canvas.transform.childCount;
+        recipeIndex = Mathf.Clamp(index, 0, recipePanels.Length - 1);
 
-            // Hide the current child
-            Canvas.transform.GetChild(index).gameObject.SetActive(false);
+        canvasRoot.SetActive(true);
+        for (int i = 0; i < recipePanels.Length; i++)
+            recipePanels[i].SetActive(i == recipeIndex);
+    }
 
-            // Move to next index (wrap around)
-            index = (index + 1) % count;
+    public void OnIngredientAdded(Recipes.IngredientType ingredient)
+    {
+        // For now just ensure UI is visible.
+        // Later you can update TMP text/icons within the active panel.
+        canvasRoot.SetActive(true);
+    }
 
-            // Show the new child
-            Canvas.transform.GetChild(index).gameObject.SetActive(true);
-
-            Debug.Log("Showing child index: " + index);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Z)) //test input
-        {
-            Canvas.SetActive(true);
-
-            // Hide the current child
-            Canvas.transform.GetChild(index).gameObject.SetActive(false);
-
-            // Move to next index (wrap around)
-
-            // Show the new child
-            Canvas.transform.GetChild(3).gameObject.SetActive(true);
-
-            Debug.Log("Showing child index: " + 3);
-        }
-
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Canvas.SetActive(false);
-        }
+    public void OnRecipeCompletedAdvance()
+    {
+        ShowRecipe(recipeIndex + 1);
     }
 }
